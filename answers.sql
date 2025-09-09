522ce6ade470b7286be3d304b03a5b3f17c9aa49
HanaTeshome721 <hana2729tm@gmail.com>
2025-09-09 20:21:07 +0300

    feat:answer

---
+++ answers.sql
@@ -0,0 +1,38 @@
+SELECT
+    E.firstName,
+    E.lastName,
+    E.email,
+    E.officeCode
+FROM
+    employees AS E
+INNER JOIN
+    offices AS O
+ON
+    E.officeCode = O.officeCode;
+
+
+SELECT
+    P.productName,
+    P.productVendor,
+    P.productLine
+FROM
+    products AS P
+LEFT JOIN
+    productlines AS PL
+ON
+    P.productLine = PL.productLine;
+
+
+
+SELECT
+    O.orderDate,
+    O.shippedDate,
+    O.status,
+    O.customerNumber
+FROM
+    customers AS C
+RIGHT JOIN
+    orders AS O
+ON
+    C.customerNumber = O.customerNumber
+LIMIT 10;
